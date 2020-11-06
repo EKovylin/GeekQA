4864f8748ebf2b846de088a8d0085f8236a53f42
parallax <Krasnodarkea@gmail.com>
2020-11-06 12:27:13 +0300

    Урок 1 (Java Core)

---
+++ src/CoreLesson1/Barrier.java
@@ -0,0 +1,7 @@
+package CoreLesson1;
+
+public interface Barrier {
+    boolean run(RunningTrack track1);
+    boolean jump(Wall wall);
+
+}
---
+++ src/CoreLesson1/Cat.java
@@ -0,0 +1,32 @@
+package CoreLesson1;
+
+public class Cat implements Barrier {
+    int maxSpeed;
+    int maxHeight;
+
+    public Cat(int maxSpeed, int maxHeight) {
+
+        this.maxSpeed = maxSpeed;
+        this.maxHeight = maxHeight;
+    }
+
+    public boolean run(RunningTrack track1){
+        if (maxSpeed > track1.speed) {
+            System.out.println("Кот пробежал " + track1.speed + "м");
+            return true;
+        } else {
+            System.out.println("Кот не смог пробежать " + track1.speed + "м");
+            return false;
+        }
+    }
+    public boolean jump(Wall wall){
+        if (maxHeight > wall.height) {
+            System.out.println("Кот прыгнул через стену " + wall.height + "м");
+            return true;
+        } else {
+            System.out.println("Кот не смог перепрыгнуть через стену " + wall.height + "м");
+            return false;
+        }
+    }
+
+}
